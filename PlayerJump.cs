fd28236c6d6ef737caf6e04f90047801bf39b8f8
MayorinBR <mayorinbr@gmail.com>
2023-03-26 21:07:17 +0900

    PlayerJump
    Player vertical movement (Jump).
    Check if player is on the ground.
    Jump height control.

---
+++ PlayerJump.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Header("Jump Details")]
+    public float jumpForce;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJump;
+
+    [Header("Ground Details")]
+    [SerializeField] private Transform groundcheck;
+    [SerializeField] private float circleRad;
+    [SerializeField] private LayerMask ground;
+    public bool grounded;
+
+    [Header("Components")]
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
+    }
+
+    private void Update()
+    {
+        grounded = Physics2D.OverlapCircle(groundcheck.position,circleRad,ground);
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJump = false;
+        }
+
+        if (Input.GetButton("Jump") && !stoppedJump && (jumpTimeCounter > 0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJump = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundcheck.position, circleRad);
+    }
+}
